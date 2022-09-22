7a3d2132589740afc6016b8245a07e954981a976
Moldoveanu Mara <mara_moldoveanu@yahoo.ro>
2022-09-22 13:28:18 +0300

    initial skeleton backend

---
+++ connectville-backend/src/main/java/com/db/connectville/ConnectvilleApplication.java
@@ -0,0 +1,13 @@
+package com.db.connectville;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConnectvilleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConnectvilleApplication.class, args);
+	}
+
+}
---
+++ connectville-backend/src/test/java/com/db/connectville/ConnectvilleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.db.connectville;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConnectvilleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
